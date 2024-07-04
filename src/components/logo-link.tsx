37dbb7ba2f314f1fbeb1a5f96586ac3cf239f55e
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-04 16:27:49 +0200

    speed up logo animation duration

--- src/components/logo-link.tsx
+++ src/components/logo-link.tsx
@@ -6,7 +6,7 @@ export function LogoLink() {
       <img
         src='/src/assets/logo.png'
         alt='Flair logo'
-        className='w-8 transition-transform duration-500 group-hover:rotate-90'
+        className='w-8 transition-transform duration-300 group-hover:rotate-90'
       />
       <h1 className='text-4xl font-semibold tracking-tight ml-3'>Flair</h1>
     </Link>
