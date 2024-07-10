dc46a7ed99aa1fbe0b3579c0f7af3dc55ecb3a1b
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-10 13:14:33 +0200

    refactor code style

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,50 +7,36 @@ export class HttpError extends Error {
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-const request = async (resource: string, init?: RequestInit): Promise<Response> => {
-  const url = `${API_BASE_URL}${resource}`;
+const request = async (resource: string, init?: RequestInit) => {
+  const url = API_BASE_URL + resource;
 
-  const defaultHeaders = {
-    'Content-Type': 'application/json',
-  };
-  const headers = {...defaultHeaders, ...init?.headers};
+  const headers = {'Content-Type': 'application/json', ...init?.headers};
 
-  const response = await fetch(url, {
-    ...init,
-    headers,
-    credentials: 'include',
-  });
-
+  const response = await fetch(url, {headers, credentials: 'include', ...init});
   if (!response.ok) {
     throw new HttpError(response.status);
   }
   return response;
 };
 
-const get = (resource: string, init?: RequestInit): Promise<Response> => {
+const get = (resource: string, init?: RequestInit) => {
   return request(resource, {...init, method: 'GET'});
 };
 
-const post = (resource: string, body?: BodyInit, init?: RequestInit): Promise<Response> => {
+const post = (resource: string, body?: BodyInit, init?: RequestInit) => {
   return request(resource, {...init, method: 'POST', body});
 };
 
-const put = (resource: string, body?: BodyInit, init?: RequestInit): Promise<Response> => {
+const put = (resource: string, body?: BodyInit, init?: RequestInit) => {
   return request(resource, {...init, method: 'PUT', body});
 };
 
-const patch = (resource: string, body?: BodyInit, init?: RequestInit): Promise<Response> => {
+const patch = (resource: string, body?: BodyInit, init?: RequestInit) => {
   return request(resource, {...init, method: 'PATCH', body});
 };
 
-const _delete = (resource: string, init?: RequestInit | null): Promise<Response> => {
+const _delete = (resource: string, init?: RequestInit | null) => {
   return request(resource, {...init, method: 'DELETE'});
 };
 
-export const api = {
-  get,
-  post,
-  put,
-  patch,
-  delete: _delete,
-};
+export const api = {get, post, put, patch, delete: _delete};
