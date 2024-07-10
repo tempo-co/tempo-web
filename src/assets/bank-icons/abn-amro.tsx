09f11132c3419f79769640681cf9aebf9669cf60
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-10 02:14:35 +0200

    add svg bank icons and their react components

---
+++ src/assets/bank-icons/abn-amro.tsx
@@ -0,0 +1,11 @@
+import {SVGProps} from 'react';
+const AbnAmro = (props: SVGProps<SVGSVGElement>) => (
+  <svg {...props} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+    <title>{'ABN AMRO'}</title>
+    <g fillRule='evenodd' clipRule='evenodd'>
+      <path fill='#1b9b94' d='M2.985 0h18.03v15.034l-8.966-8.946V24l-9.064-8.966Z' />
+      <path fill='#ecb73a' d='M12.049 24V6.088l8.966 8.946z' />
+    </g>
+  </svg>
+);
+export default AbnAmro;
