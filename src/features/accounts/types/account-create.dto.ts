9ae67e30ed6ab6e87fa6ee183caba4a76059ad00
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-13 14:43:49 +0200

    update error message

--- src/features/accounts/types/account-create.dto.ts
+++ src/features/accounts/types/account-create.dto.ts
@@ -4,7 +4,7 @@ import {Bank} from '@/types/bank';
 
 export const accountCreateDtoSchema = z.object({
   alias: z.string().max(50, 'Alias must be less than 50 characters.').optional(),
-  bank: z.nativeEnum(Bank, {message: 'Bank is required.'}),
+  bank: z.nativeEnum(Bank, {message: 'Please select a bank.'}),
 });
 
 export type AccountCreateDto = z.infer<typeof accountCreateDtoSchema>;
