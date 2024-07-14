238e2585a28ffb1f83a9f5b0ebd4730d3cabc905
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-14 12:32:38 +0200

    rename and store open state in localStorage; map over links from array

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,6 +1,6 @@
 import {Link} from '@tanstack/react-router';
 import {ChevronLeft, CreditCard, LayoutGrid, LogOut, Settings, WalletCards} from 'lucide-react';
-import {useState} from 'react';
+import {useEffect} from 'react';
 
 import {Avatar, AvatarFallback} from '@/components/ui/avatar';
 import {Button} from '@/components/ui/button';
@@ -8,17 +8,24 @@ import {Popover, PopoverContent, PopoverTrigger} from '@/components/ui/popover';
 import {Separator} from '@/components/ui/separator';
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from '@/components/ui/tooltip';
 import {useCurrentUser} from '@/hooks/use-current-user';
+import {useLocalStorage} from '@/hooks/use-local-storage';
 import {useLogOut} from '@/hooks/use-logout';
 import {cn} from '@/utils/cn';
 
+const links = [
+  {to: '/dashboard', label: 'Dashboard', icon: LayoutGrid},
+  {to: '/accounts', label: 'Accounts', icon: WalletCards},
+  {to: '/transactions', label: 'Transactions', icon: CreditCard},
+];
+
 export function SideBar() {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isOpen, setIsOpen] = useLocalStorage<boolean>('sidebar', false);
   const {currentUser} = useCurrentUser();
   const logOut = useLogOut();
 
-  const handleLogOut = () => {
-    logOut();
-  };
+  useEffect(() => {
+    localStorage.setItem('sidebar', JSON.stringify(isOpen));
+  }, [isOpen]);
 
   return (
     <TooltipProvider delayDuration={0}>
@@ -26,12 +33,12 @@ export function SideBar() {
         <div
           className={cn(
             'bg-card px-3 py-4 h-screen flex flex-col justify-between transition-all duration-100',
-            isCollapsed ? 'w-16' : 'w-52',
+            isOpen ? 'w-52' : 'w-16',
           )}
         >
           <div>
             <div className='flex justify-between transition-all duration-1000'>
-              {!isCollapsed && (
+              {isOpen && (
                 <Button asChild variant='ghost'>
                   <Link to='/' className='flex items-center group w-fit'>
                     <img
@@ -43,11 +50,11 @@ export function SideBar() {
                   </Link>
                 </Button>
               )}
-              <Button onClick={() => setIsCollapsed(!isCollapsed)} size='icon' variant='ghost'>
+              <Button onClick={() => setIsOpen(!isOpen)} size='icon' variant='ghost'>
                 <div
                   className={cn(
                     'transform transition-transform duration-200',
-                    isCollapsed && 'rotate-180',
+                    !isOpen && 'rotate-180',
                   )}
                 >
                   <ChevronLeft />
@@ -56,60 +63,24 @@ export function SideBar() {
             </div>
             <Separator className='my-4' />
             <div className='grid gap-2 items-start'>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link to='/'>
+              {links.map(({to, label, icon: Icon}) => (
+                <Tooltip key={to}>
+                  <TooltipTrigger asChild>
                     <Button
                       variant='ghost'
-                      size={isCollapsed ? 'icon' : 'default'}
-                      className={cn(
-                        'gap-2 w-full',
-                        isCollapsed ? 'justify-center' : 'justify-start',
-                      )}
+                      size={isOpen ? 'default' : 'icon'}
+                      className={cn('gap-2 w-full', isOpen ? 'justify-start' : 'justify-center')}
+                      asChild
                     >
-                      <LayoutGrid className='w-5 h-5' />
-                      {!isCollapsed && 'Dashboard'}
+                      <Link to={to} activeProps={{className: 'bg-secondary rounded-md'}}>
+                        <Icon className='w-5 h-5' />
+                        {isOpen && label}
+                      </Link>
                     </Button>
-                  </Link>
-                </TooltipTrigger>
-                {isCollapsed && <TooltipContent side='right'>Dashboard</TooltipContent>}
-              </Tooltip>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link to='/accounts'>
-                    <Button
-                      variant='ghost'
-                      size={isCollapsed ? 'icon' : 'default'}
-                      className={cn(
-                        'gap-2 w-full',
-                        isCollapsed ? 'justify-center' : 'justify-start',
-                      )}
-                    >
-                      <WalletCards className='w-5 h-5' />
-                      {!isCollapsed && 'Accounts'}
-                    </Button>
-                  </Link>
-                </TooltipTrigger>
-                {isCollapsed && <TooltipContent side='right'>Accounts</TooltipContent>}
-              </Tooltip>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link to='/transactions'>
-                    <Button
-                      variant='ghost'
-                      size={isCollapsed ? 'icon' : 'default'}
-                      className={cn(
-                        'gap-2 w-full',
-                        isCollapsed ? 'justify-center' : 'justify-start',
-                      )}
-                    >
-                      <CreditCard className='w-5 h-5' />
-                      {!isCollapsed && 'Transactions'}
-                    </Button>
-                  </Link>
-                </TooltipTrigger>
-                {isCollapsed && <TooltipContent side='right'>Transactions</TooltipContent>}
-              </Tooltip>
+                  </TooltipTrigger>
+                  {!isOpen && <TooltipContent side='right'>{label}</TooltipContent>}
+                </Tooltip>
+              ))}
             </div>
           </div>
           <div>
@@ -122,13 +93,13 @@ export function SideBar() {
                       variant='ghost'
                       className={cn(
                         'flex items-center gap-2 text-sm w-full',
-                        isCollapsed ? 'w-10 rounded-full px-4 py-4' : 'justify-start px-1',
+                        isOpen ? 'justify-start px-1' : 'w-10 rounded-full px-4 py-4',
                       )}
                     >
                       <Avatar className='h-8 w-8'>
                         <AvatarFallback>JD</AvatarFallback>
                       </Avatar>
-                      {!isCollapsed && <p>{currentUser?.name}</p>}
+                      {isOpen && <p>{currentUser?.name}</p>}
                     </Button>
                   </TooltipTrigger>
                 </PopoverTrigger>
@@ -136,20 +107,24 @@ export function SideBar() {
                   <div
                     className={cn(
                       'flex flex-col gap-2 w-[--radix-popover-trigger-width] !p-2 mb-1',
-                      isCollapsed && 'w-44 ml-2',
+                      !isOpen && 'w-44 ml-2',
                     )}
                   >
                     <Button variant='ghost' className='w-full justify-start'>
                       <Settings className='mr-2 w-5 h-5' />
                       Settings
                     </Button>
-                    <Button variant='ghost' className='w-full justify-start' onClick={handleLogOut}>
+                    <Button
+                      variant='ghost'
+                      className='w-full justify-start'
+                      onClick={() => logOut()}
+                    >
                       <LogOut className='mr-2 w-5 h-5' />
                       Log out
                     </Button>
                   </div>
                 </PopoverContent>
-                {isCollapsed && <TooltipContent side='right'>{currentUser?.name}</TooltipContent>}
+                {!isOpen && <TooltipContent side='right'>{currentUser?.name}</TooltipContent>}
               </Tooltip>
             </Popover>
           </div>
