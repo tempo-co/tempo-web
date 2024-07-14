777f2bcf35a505387047af101c640e55953ab267
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-14 13:22:38 +0200

    add isAuthenticated check on beforeLoad

--- src/routes/transactions.tsx
+++ src/routes/transactions.tsx
@@ -1,7 +1,12 @@
-import {createFileRoute} from '@tanstack/react-router';
+import {createFileRoute, redirect} from '@tanstack/react-router';
 
 export const Route = createFileRoute('/transactions')({
   component: Transactions,
+  beforeLoad: ({context, location}) => {
+    if (!context.isAuthenticated) {
+      throw redirect({to: '/login', search: {redirect: location.href}});
+    }
+  },
 });
 
 function Transactions() {
