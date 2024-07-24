c99cc18fafe349c03c28e4f109e59ac34477ebe2
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-25 00:46:03 +0200

    update util func to accept optional maxLength param

--- src/features/bank-statements/utils/truncate-file-name.ts
+++ src/features/bank-statements/utils/truncate-file-name.ts
@@ -1,12 +1,12 @@
 const MAX_LENGTH = 70;
 
-export function truncateFileName(name: string) {
+export function truncateFileName(name: string, maxLength: number = MAX_LENGTH) {
   const lastDotIndex = name.lastIndexOf('.');
   const extension = lastDotIndex !== -1 ? name.substring(lastDotIndex) : '';
   const baseName = lastDotIndex !== -1 ? name.substring(0, lastDotIndex) : name;
 
-  if (name.length > MAX_LENGTH) {
-    const truncationLength = MAX_LENGTH - extension.length - 3;
+  if (name.length > maxLength) {
+    const truncationLength = maxLength - extension.length - 3;
     return `${baseName.substring(0, truncationLength > 0 ? truncationLength : 0)}..${extension}`;
   }
   return name;
