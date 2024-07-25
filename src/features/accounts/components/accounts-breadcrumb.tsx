cd637c71212dadca4106a84a05921c3fc2e06c95
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-25 17:27:07 +0200

    add 'Bank Statements' breadcrumb link

--- src/features/accounts/components/accounts-breadcrumb.tsx
+++ src/features/accounts/components/accounts-breadcrumb.tsx
@@ -9,12 +9,15 @@ import {
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
 import {Account} from '@/types/account';
+import {BankStatement} from '@/types/bank-statement';
 
 type AccountsBreadcrumbProps = {
   account?: Account;
+  bankStatements?: boolean;
+  bankStatement?: BankStatement;
 };
 
-export function AccountsBreadcrumb({account}: AccountsBreadcrumbProps) {
+export function AccountsBreadcrumb({account, bankStatements}: AccountsBreadcrumbProps) {
   return (
     <Breadcrumb className='pb-5'>
       <BreadcrumbList>
@@ -33,11 +36,25 @@ export function AccountsBreadcrumb({account}: AccountsBreadcrumbProps) {
             <BreadcrumbPage>Accounts</BreadcrumbPage>
           </BreadcrumbItem>
         )}
-        {account && (
+        {account && !bankStatements && (
           <>
             <BreadcrumbSeparator />
             <BreadcrumbItem>
               <BreadcrumbPage>{account.alias ? account.alias : account.bank}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </>
+        )}
+        {account && bankStatements && (
+          <>
+            <BreadcrumbSeparator />
+            <BreadcrumbLink asChild>
+              <Link to={`/accounts/${account.id}`}>
+                {account.alias ? account.alias : account.bank}
+              </Link>
+            </BreadcrumbLink>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Bank Statements</BreadcrumbPage>
             </BreadcrumbItem>
           </>
         )}
