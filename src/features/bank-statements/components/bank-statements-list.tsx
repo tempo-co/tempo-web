b5edc89a3fb9a594da55cfa074c179d727a4dd75
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-25 17:29:39 +0200

    update bank-statements-list component

--- src/features/bank-statements/components/bank-statements-list.tsx
+++ src/features/bank-statements/components/bank-statements-list.tsx
@@ -1,12 +1,33 @@
-import {AccountsBreadcrumb} from '@/features/accounts/components/accounts-breadcrumb';
+import {useParams} from '@tanstack/react-router';
 
-import {BankStatementUploadInput} from './bank-statement-upload-input';
+import {useGetAccount} from '@/features/accounts/api/use-get-account';
+import {AccountsBreadcrumb} from '@/features/accounts/components/accounts-breadcrumb';
+import {useGetAllBankStatements} from '@/features/bank-statements/api/use-get-all-bank-statements';
+import {BankStatementUploadInput} from '@/features/bank-statements/components/bank-statement-upload-input';
+
+import {BankStatementsTable} from './bank-statements-table';
 
 export function BankStatementsList() {
+  const {accountId} = useParams({from: '/accounts/$accountId/bank-statements'});
+
+  const {account, isPending: isAccountPending} = useGetAccount(accountId);
+  const {bankStatements, isPending: isBankStatementsPending} = useGetAllBankStatements(accountId);
+
   return (
-    <>
-      <AccountsBreadcrumb />
+    <div className='max-w-4xl'>
+      {isAccountPending && <AccountsBreadcrumb />}
+      {account && <AccountsBreadcrumb account={account} bankStatements />}
+      {isBankStatementsPending && <p>Loading bank statements...</p>}
+      {bankStatements && (
+        <div>
+          {bankStatements.length > 0 ? (
+            <BankStatementsTable bankStatements={bankStatements} />
+          ) : (
+            <p>No bank statements uploaded yet.</p>
+          )}
+        </div>
+      )}
       <BankStatementUploadInput />
-    </>
+    </div>
   );
 }
