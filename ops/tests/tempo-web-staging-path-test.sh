3d83a95ad500e0d59de64ceafd9ca2343f6b20b1
eduard-cc <114700003+eduard-cc@users.noreply.github.com>
2026-09-24 00:12:47 +0200

    feat(email): remove production Mailpit proxy routes

--- ops/tests/tempo-web-staging-path-test.sh
+++ ops/tests/tempo-web-staging-path-test.sh
@@ -14,6 +14,9 @@ for fragment in ['location = /tempo {', 'location /tempo/api/ {', 'location /tem
 for fragment in ['absolute_redirect off;', 'location = /staging {', 'location = /staging/mailpit {', 'location /staging/mailpit/ {', 'location = /staging/bank-connections/callback {', 'location = /staging/api {', 'location /staging/api/ {', 'location /staging/ {', 'proxy_pass http://mailpit:8025/staging/mailpit/;', 'rewrite ^/staging/(.*)$ /$1 break;', 'rewrite ^/staging/api/?(.*)$ /$1 break;']:
     assert fragment in staging, fragment
 assert 'location /staging/' not in production
+assert 'mailpit' not in production
+assert 'location = /staging/mailpit {' in staging
+assert 'location /staging/mailpit/ {' in staging
 staging_spa = staging.split('location /staging/ {', 1)[1].split('location / {', 1)[0]
 assert 'try_files $uri /index.html;' in staging_spa
 assert 'try_files $uri $uri/ /index.html;' not in staging_spa
